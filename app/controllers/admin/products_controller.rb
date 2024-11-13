d7f2b6e7b1dc2d474700a1a8629b79c84ac6f739
amusictheorist <a_millaresthomson@outlook.com>
2024-11-13 15:54:49 -0500

    Implement http basic authenticate

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,11 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["HTTP_BASIC_USERNAME"] && password == ENV["HTTP_BASIC_PASSWORD"]
+    end
+  end
 
   def index
     @products = Product.order(id: :desc).all
